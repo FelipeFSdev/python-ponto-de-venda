313a8b181e1f8f2784aa9e7f488acc9f06865b68
felipe <felipe.a.melo@hotmail.com>
2026-04-14 08:11:57 -0300

    adding users route in main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
 from src.infra.database.session import create_tables
+from src.routes.category_router import router as category_router
+from src.routes.user_router import router as user_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -11,3 +13,6 @@ async def lifespan(app: FastAPI):
     print("server's down")
 
 app = FastAPI(lifespan=lifespan)
+
+app.include_router(category_router)
+app.include_router(user_router)
